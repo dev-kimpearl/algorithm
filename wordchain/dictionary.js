942b46b541b13ef1d7f4e025afc39a642fefb20e
Bazzi <dev.kimpearl@gmail.com>
2020-10-05 20:54:25 +0900

    * 끝말잇기 만들기 * 출처 : zerochoTV - ES2020 javascript 기초 강의 - https://www.youtube.com/watch?v=q3RDTYLIzxo&list=PLcqDmjxt30RuMjo2hbuUmc1xzPoEGnLdz

---
+++ wordchain/dictionary.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector('#button');
+const word = document.querySelector('#word');
+const userWord = document.querySelector('#userWord');
+const error = document.querySelector('#error');
+const dictionary = [];
+
+btn.addEventListener('click', () => {
+    const word_value = word.textContent;
+    const userWord_value = userWord.value;
+    const last_char = word_value[word_value.length -1];
+    const first_char = userWord_value[0];
+        if (dictionary.includes(userWord_value)) {
+            error.innerText = '중복입니다!'
+            userWord.value = '';
+        } else {
+            if (last_char === first_char) {
+                word.innerText = userWord_value;
+                error.innerText = '';
+                userWord.value = '';
+                userWord.focus(); // 커서가 input에 유지되도록 설정
+                dictionary.push(userWord_value);
+            } else {
+                error.innerText = '땡!'
+                userWord.value = '';
+                userWord.focus(); // 커서가 input에 유지되도록 설정
+            }
+        } 
+    
+})
+
